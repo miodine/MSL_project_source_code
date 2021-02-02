88aa4bacc512031ff4d4d8ed1406476e24f5b84f
keshavharits123 <49718755+keshavharits123@users.noreply.github.com>
2021-02-02 22:31:02 +0100

    stm32 project

---
+++ LAB_4/Core/Src/MS_UART_trns.c
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @file    MS_UART_trns.c
+  * @author  Paweł Rozenblut
+  * @brief   Bodies of the functions realisng data transmission/receiving.
+  *   
+  ******************************************************************************
+  */
+
+
+#include "MS_UART_trns.h"
+#include "MS_core.h"
+
+void transmit_data_packet(Program_Data* pd)
+{
+  HAL_UART_Transmit(&huart3, (uint8_t*)&(pd->tx_flag), 1, 1000);
+
+  uint8_t a[4] = {0,};
+
+	memcpy(a, &(pd->RPM_actual), sizeof(pd->RPM_actual));
+	HAL_UART_Transmit(&huart3, (uint8_t*)a, 4, 1000);
+  
+  memcpy(a, &(pd->RPM_reference), sizeof(pd->RPM_reference));
+	HAL_UART_Transmit(&huart3, (uint8_t*)a, 4, 1000);
+}
+
+
+void deparse_received_data(Program_Data* pd)
+{
+    pd->RPM_reference = pd->rx_buffer[12] | (pd->rx_buffer[13] << 8) | (pd->rx_buffer[14] << 16) | (pd->rx_buffer[15] << 24);
+}
+
